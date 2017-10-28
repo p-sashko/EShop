2662783ac818ed54ebd0413e64b8431c886b40f8
EShop <p-sashko@mail.ru>
2017-10-29 00:33:53 +0300

    ADD DAL

---
+++ EShop.DAL/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.DAL
+{
+    class Product : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Price { get; set; }
+
+    }
+}
