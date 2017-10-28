1fa525bcaff9925c4e397f24bb7a5c05c5157c82
EShop <p-sashko@mail.ru>
2017-10-28 23:51:39 +0300

    test repo

--- EShop/Controllers/HomeController.cs
+++ EShop/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace EShop.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page. 111";
 
             return View();
         }
